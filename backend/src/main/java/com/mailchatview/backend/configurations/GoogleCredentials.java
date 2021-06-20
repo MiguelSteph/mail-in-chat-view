8e47154958ce70920cfef91b4bfc856cec088d4b
Miguel <because_of_peace@yahoo.fr>
2021-06-20 22:46:06 +0100

    Configure google credentials

---
+++ backend/src/main/java/com/mailchatview/backend/configurations/GoogleCredentials.java
@@ -0,0 +1,28 @@
+package com.mailchatview.backend.configurations;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import java.util.List;
+
+@Configuration
+@PropertySource("classpath:google-credentials.properties")
+@ConfigurationProperties(prefix = "web")
+@Data
+public class GoogleCredentials {
+
+    @Value("${google.web.id}")
+    private String id;
+    @Value("${google.web.secret}")
+    private String secret;
+
+    private String projectId;
+    private String authUri;
+    private String tokenUri;
+    private String authProviderX509CertUrl;
+    private List<String> redirectUris;
+    private List<String> javascriptOrigins;
+}
